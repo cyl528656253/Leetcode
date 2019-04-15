622139e2da825fdc15408ac2b6cfce9b87198557
蔡于力 <528656253@qq.com>
2019-04-16 01:14:50 +0800

    更新了85

--- src/leetcode/Leetcode.java
+++ src/leetcode/Leetcode.java
@@ -2598,7 +2598,6 @@ public class Leetcode {
 
             }
         }
-
         int count = 1;
         while(!sortStack.isEmpty())
         {
@@ -2607,7 +2606,67 @@ public class Leetcode {
             count ++;
         }
         return result;
+    }
 
+    //85. Maximal Rectangle
+
+    /**
+     * 思路一  根据84的思路  把二维合成一维  在每一行（与这一行往上的和）求他和的加和  使用84的算法
+     *
+     * @param matrix
+     * @return
+     */
+    public int maximalRectangle(char[][] matrix) {
+        if(matrix == null || matrix.length == 0 || matrix[0] == null) return 0;
+
+        int m = matrix.length, n = matrix[0].length;
+        int[] l = new int[n];
+        int[] r = new int[n];
+        int[] h = new int[n];
+        int result = 0;
+
+        for(int i = 0; i < n; i++){
+            l[i] = 0;
+            r[i] = n;
+            h[i] = 0;
+        }
+        for(int i = 0; i < m; i++){
+            int cur_left = 0, cur_right = n;
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == '1') h[j] += 1;
+                else                    h[j] = 0;
+//              System.out.print(h[j]);
+//              System.out.print(" ");
+            }
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == '1'){
+                    l[j] = Math.max(l[j], cur_left);
+                }
+                else{
+                    l[j] = 0;
+                    cur_left = j + 1;
+                }
+//              System.out.print(l[j]);
+//              System.out.print(" ");
+            }
+            for(int j = n-1; j >= 0; j--){
+                if(matrix[i][j] == '1'){
+                    r[j] = Math.min(r[j], cur_right);
+                }
+                else{
+                    r[j] = n;
+                    cur_right = j;
+                }
+//              System.out.print(r[j]);
+//              System.out.print(" ");
+            }
+            for(int j = 0; j < n; j++){
+                result = Math.max(result, (r[j] - l[j]) * h[j]);
+            }
+            System.out.println();
+        }
+
+        return result;
 
     }
 
